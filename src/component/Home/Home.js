ffbbc3f351b9b0b73ddc5e4118d8b62cd665ad38
ssmdbayzid <ssmd.bayzid@gmail.com>
2022-04-18 06:39:45 +0600

    added Addition Section

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Services from '../Services/Services'
+import Services from '../Services/Services';
 import './Home.css'
 
 
@@ -25,6 +25,17 @@ const Home = () => {
                 {
                     services.map(service => <Services key={service.id} service={service}></Services>)
                 }
+            </section>
+            <section>
+                <div>
+                    <hr/>
+                    <h1 style={{color: 'brown'}}>Save Palestine</h1>
+                    <hr />
+                    <img src="./palestine.jpg" alt="" srcset="" />
+                <h5 className='w-75 mx-auto'>This time it is different – the Palestinian struggle for equality and freedom has reached a new and radical stage.
+
+In the face of Israel’s most recent assault on their rights and lives in occupied East Jerusalem and Gaza, the Palestinians not only demonstrated unprecedented courage and unity, but also embarked on a landmark general strike, the first in decades, with the full support of both Fatah and Hamas.</h5>
+                </div>
             </section>
 
         </div>
